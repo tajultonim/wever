927de0e2dbc4a3bc83387661571554d3afea8010
tajultonim <mail@tajultonim.com>
2023-10-31 02:13:18 +0600

    select none added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
+  useEffect(() => {
+    window.oncontextmenu = function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      return false;
+    };
+  });
   return (
     <>
       <SessionProvider session={session}>
